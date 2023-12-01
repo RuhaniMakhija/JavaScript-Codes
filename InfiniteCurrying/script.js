599a538b210028293421a4168d57f2f26b8c2dda
Ruhani FashionFantasy <ruhanifashionfantasy@Ruhanis-MacBook-Air.local>
2023-12-01 16:06:10 +0530

    Infinite Currying

---
+++ InfiniteCurrying/script.js
@@ -0,0 +1,21 @@
+function sum(a){
+    return function(b){
+        if(b){
+            return sum(a+b);
+        }else{
+            return a
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+console.log(sum(1)(2)(5)(7)());
